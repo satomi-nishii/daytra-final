f8de9b9271785b677b8a116c9628a5354a0bfc0a
satomi-nishii <70808493+satomi-nishii@users.noreply.github.com>
2020-11-24 00:17:32 +0900

    下層2Pの静的コーディング完了
    ・inquiry
    ・news-single
    （残り1ページ！）

--- js/script.js
+++ js/script.js
@@ -6,4 +6,21 @@ $(function() {
     // wow
     new WOW().init();
 
+    // formの入力確認
+    let $submit = $('#js-submit')
+    $('#js-form input, #js-form textarea').on('change', function() {
+        if (
+            $('#js-form input[type="text"]').val() !== "" &&
+            $('#js-form input[type="email"]').val() !== ""
+        ){
+            // 全て入力された時
+            $submit.prop('disabled', false)
+            $submit.addClass('-active')
+        } else {
+            // 全て入力されていない時
+            $submit.prop('disabled', true)
+            $submit.removeClass('-active')
+        }
+    });
+    
 });
